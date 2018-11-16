a83b1b7eed8d33229bce5e5405a840da34645739
Michaela D Shoffner <mshoffn2@vols.utk.edu>
2018-11-16 11:21:48 -0500

    almost

--- rsa-enc.py
+++ rsa-enc.py
@@ -131,8 +131,8 @@ size_N = int(size_N)
 result = rsa_enc(msg_txt, main_N, main_e, size_N)
 
 #open and write to output file
-end_file = open(out_f_name, "w")
-end_file.write(str(result))
+end_file = open(out_f_name, "wb")
+end_file.write(result)
 end_file.close()
 
 
