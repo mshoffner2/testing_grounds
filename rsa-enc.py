634df498ae096b8dc155894e44a5a1ff22e9b3bb
Michaela D Shoffner <mshoffn2@vols.utk.edu>
2018-11-16 12:21:20 -0500

    here

--- rsa-enc.py
+++ rsa-enc.py
@@ -47,18 +47,18 @@ def rsa_enc(message, N, e, size_N):
     #print hex(total_r)
     total_r *= 2**8
     temp = hex(total_r)[2:]
-    print temp
+    print(temp)
 
     #making space for the message at the end of total_r
     total_r *= 2**(len(message) * 8)
 
     temp = hex(total_r)[2:]
-    print temp
+    print(temp)
     
     #adding message to the end. Since message is an AES key, we assume that it's in hex in the file
     padded_msg = total_r + int(message, 16)
     temp = hex(padded_msg)[2:]
-    print temp
+    print(temp)
     
     #find the cipher
     cipher_result = mod_ex(padded_msg, e, N)
