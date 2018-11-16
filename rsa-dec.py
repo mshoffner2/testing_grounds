307413a8bd0e8312e37ea055800f61c12927ade9
Michaela D Shoffner <mshoffn2@vols.utk.edu>
2018-11-16 13:06:55 -0500

    blah

--- rsa-dec.py
+++ rsa-dec.py
@@ -54,13 +54,13 @@ def rsa_dec(ciphertext, N, d, size_N):
     #change back to hex, remove extra characters added
     result = padded_msg
     result = hex(result)[2:]
-    i =  0
+    i = len(result)-1
     keep_going = 1
 
     while(keep_going == 1 and i < len(result) - 1):
         if(result[i] == '0' and result[i + 1] == '0'):
             keep_going = 0
-        i = i + 1
+        i = i - 1
     result = result[i:]
     result = int(result, 16)
     result = hex(result)[2:]
